225f521130b29e37a7d2f376a429b3f1fb93d455
Lucas Foseca <lucasfonmiranda@gmail.com>
2017-05-28 15:03:35 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+a=str(input('Qual o turno: '))
+if a=='m' or a=='M':
+	print('bom dia')
+elif a=='v' or a=='V':
+	print('Boa tarde')
+elif a=='n' or a=='N':
+	print('boa noite')
+else:
+	print('argumento invalido')
